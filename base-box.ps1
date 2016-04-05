cde30889b7e4d21b0b9514b6c24aea57e04d28b1
neutmute <neutmute@users.noreply.github.com>
2016-04-05 20:49:11 +1000

    silence

--- base-box.ps1
+++ base-box.ps1
@@ -6,7 +6,7 @@ $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$false
 $Boxstarter.AutoLogin=$true
 
-Enable-MicrosoftUpdate
+
 Enable-RemoteDesktop
 Update-ExecutionPolicy -Policy Unrestricted
 
@@ -15,24 +15,38 @@ Set-Volume -DriveLetter $env:SystemDrive[0] -NewFileSystemLabel "System"
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
 Set-TaskbarOptions -Combine Never
 
-Write-BoxstarterMessage "Starting chocolatey installs"
+function InstallChocoCoreApps()
+{
+	Write-BoxstarterMessage "Starting chocolatey installs"
+	
+	choco install firefox                           --yes --limitoutput
+	choco install googlechrome                      --yes --limitoutput
+	choco install flashplayerplugin                 --yes --limitoutput
+	choco install notepadplusplus.install           --yes --limitoutput
+	choco install paint.net                         --yes --limitoutput
+	choco install itunes                            --yes --limitoutput
+	choco install irfanview                         --yes --limitoutput
+	choco install irfanviewplugins                  --yes --limitoutput
+	choco install 7zip.install                      --yes --limitoutput
+	choco install k-litecodecpackfull               --yes --limitoutput
+	#choco install veracrypt 						--yes --limitoutput #not silent
+}
 
-choco install firefox                           --yes
-choco install googlechrome                      --yes
-choco install flashplayerplugin                 --yes
-choco install notepadplusplus.install           --yes
-choco install paint.net                         --yes
-choco install itunes                            --yes
-choco install irfanview                         --yes
-choco install irfanviewplugins                  --yes
-choco install 7zip.install                      --yes
-choco install k-litecodecpackfull               --yes
-choco install veracrypt 						--yes
+function InstallChocoUserSettings()
+{
+	choco install commandwindowhere                 --yes --limitoutput
+	choco install taskbar-never-combine             --yes --limitoutput
+	choco install explorer-show-all-folders         --yes --limitoutput
+	choco install explorer-expand-to-current-folder --yes --limitoutput
+}
 
-choco install commandwindowhere                 --yes
-choco install taskbar-never-combine             --yes
-choco install explorer-show-all-folders         --yes
-choco install explorer-expand-to-current-folder --yes
+function InstallWindowsUpdate()
+{
+	Enable-MicrosoftUpdate
+	#Install-WindowsUpdate -AcceptEula
+	if (Test-PendingReboot) { Invoke-Reboot }
+}
 
-#Install-WindowsUpdate -AcceptEula
-if (Test-PendingReboot) { Invoke-Reboot }
+InstallChocoCoreApps
+InstallChocoUserSettings
+InstallWindowsUpdate
