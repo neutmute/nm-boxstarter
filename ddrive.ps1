94646f9fc772515227b607b2cd3a275b9459abcc
neutmute <neutmute@users.noreply.github.com>
2017-05-02 18:42:05 +1000

    force folder to create

--- ddrive.ps1
+++ ddrive.ps1
@@ -10,6 +10,13 @@ Set-Volume -DriveLetter "D" -NewFileSystemLabel "Data"
 $userDataPath = "D:\Data\Documents"
 $mediaPath    = "D:\Media"
 
+New-Item -ItemType Directory -Force -Path (Join-Path $userDataPath "Pictures") | Out-Null
+New-Item -ItemType Directory -Force -Path (Join-Path $userDataPath "Documents") | Out-Null
+New-Item -ItemType Directory -Force -Path (Join-Path $userDataPath "Desktop") | Out-Null
+New-Item -ItemType Directory -Force -Path (Join-Path $mediaPath "Videos") | Out-Null
+New-Item -ItemType Directory -Force -Path (Join-Path $mediaPath "Music") | Out-Null
+New-Item -ItemType Directory -Force -Path "D:\Downloads" | Out-Null
+
 Set-KnownFolderPath -KnownFolder 'Pictures'     -Path (Join-Path $userDataPath "Pictures")
 Set-KnownFolderPath -KnownFolder 'Documents'    -Path (Join-Path $userDataPath "Documents")
 Set-KnownFolderPath -KnownFolder 'Desktop'      -Path (Join-Path $userDataPath "Desktop")
